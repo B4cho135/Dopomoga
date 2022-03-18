0404d188c4c746b52a8b0a7fc5c9bcb0e41d07a7
badri.tatarashvili@gmail.com <badri.tatarashvili@gmail.com>
2022-03-19 02:06:43 +0400

    add login page

---
+++ test/Controllers/Identity/IdentityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers.Identity
+{
+    public class IdentityController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
